f491fdff835d885310483e653601619612b7e938
elangreen <green.elan5@gmail.com>
2019-07-05 17:22:23 -0700

    first draft of videos page and initial styling

--- src/routes/Videos.js
+++ src/routes/Videos.js
@@ -1,15 +1,71 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import VideoItem from '../components/VideoItem/VideoItem'
+import './Videos.css'
 
 export default class Videos extends Component {
-  componentDidMount() {
-      console.log('I was mounted')
+  constructor(props) {
+    super(props)
+    this.state = {
+      videos: [],
+      current: null
+    }
+    this.BASE_URL = 'http://localhost:8000/api'
+  }
+  async componentDidMount() {
+    const videos = await axios.get(`${this.BASE_URL}/videos`)
+    this.setState({ videos: videos.data })
+  }
+
+  renderVideos(){
+    const { videos } = this.state
+    if(videos) {
+        let videoList = [];
+        for (let i = 0; i < 4; i++) {
+            videoList.push(videos[this.state.current+i])
+        }
+        return videoList.map((video, idx) => {
+          return (
+            <VideoItem video={video} key={idx}/>
+          )
+      })
+    }
+  }
+  nextVideo() {
+    const { videos } = this.state
+    if (videos[this.state.current+3]) {
+        this.setState({
+            current:this.state.current + 1
+        })
+    }
+  }
+
+  previousVideo(){
+    const { videos } = this.state
+    if (videos[this.state.current - 1]) {
+        this.setState({
+            current: this.state.current - 1
+        })
+    }
   }
 
   render() {
 
     return (
-      <section>
-        this is videos main page
+      <section className='videos-page'>
+        <div className="btn-container">
+          <button type="button" onClick ={(ev) => {
+              ev.preventDefault()
+              this.previousVideo()
+          }}>Previous</button>
+          <button type="button" onClick ={(ev) => {
+              ev.preventDefault()
+              this.nextVideo()
+          }}>Next</button>
+        </div>
+        <div className='my-videos-container'>
+        {this.renderVideos()}
+        </div>
       </section>
     );
   }
