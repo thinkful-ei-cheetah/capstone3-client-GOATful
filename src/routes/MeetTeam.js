f8423fd50878dc035c961e40060b45c020450b5e
David Nordeen <davidnordeen@gmail.com>
2019-07-07 16:10:10 -0700

    changed names

--- src/routes/MeetTeam.js
+++ src/routes/MeetTeam.js
@@ -28,7 +28,20 @@ class Landing extends Component {
                 onMouseOut={e => (e.currentTarget.src = 'https://media.licdn.com/dms/image/C5103AQEIiVj4du7bpA/profile-displayphoto-shrink_800_800/0?e=1568246400&v=beta&t=U6TqAzgGvtL5TGHebfAwrywPbYb1nsGjea_A8_od3YM')} />
             </figure>
             <h3>
-              Kristof-Pierre Cummings
+              David K. Nordeen
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          </section>
+
+          <section className='click-through'>
+            <figure>
+              <img src="https://media.licdn.com/dms/image/C5103AQEIiVj4du7bpA/profile-displayphoto-shrink_800_800/0?e=1568246400&v=beta&t=U6TqAzgGvtL5TGHebfAwrywPbYb1nsGjea_A8_od3YM" alt="Click through rate"
+                onMouseOver={e => (e.currentTarget.src = 'https://amp.businessinsider.com/images/5c48c7ea2bdd7f581f7508a0-750-375.jpg')}
+                onMouseOut={e => (e.currentTarget.src = 'https://media.licdn.com/dms/image/C5103AQEIiVj4du7bpA/profile-displayphoto-shrink_800_800/0?e=1568246400&v=beta&t=U6TqAzgGvtL5TGHebfAwrywPbYb1nsGjea_A8_od3YM')} />
+            </figure>
+            <h3>
+              Elan Green
             </h3>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
@@ -46,7 +59,6 @@ class Landing extends Component {
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
           </section>
-
           <section className='click-through'>
             <figure>
               <img src="https://media.licdn.com/dms/image/C5103AQEIiVj4du7bpA/profile-displayphoto-shrink_800_800/0?e=1568246400&v=beta&t=U6TqAzgGvtL5TGHebfAwrywPbYb1nsGjea_A8_od3YM" alt="Click through rate"
@@ -54,7 +66,7 @@ class Landing extends Component {
                 onMouseOut={e => (e.currentTarget.src = 'https://media.licdn.com/dms/image/C5103AQEIiVj4du7bpA/profile-displayphoto-shrink_800_800/0?e=1568246400&v=beta&t=U6TqAzgGvtL5TGHebfAwrywPbYb1nsGjea_A8_od3YM')} />
             </figure>
             <h3>
-              Kristof-Pierre Cummings
+              Michael Verdi
             </h3>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
@@ -66,7 +78,7 @@ class Landing extends Component {
                 onMouseOut={e => (e.currentTarget.src = 'https://media.licdn.com/dms/image/C5103AQEIiVj4du7bpA/profile-displayphoto-shrink_800_800/0?e=1568246400&v=beta&t=U6TqAzgGvtL5TGHebfAwrywPbYb1nsGjea_A8_od3YM')} />
             </figure>
             <h3>
-              Kristof-Pierre Cummings
+              Peter Pae
             </h3>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
