7843d3600bfd370c2b1acd43c486aaf1e8951892
Peter Pae <paendabear@gmail.com>
2019-07-09 16:29:57 -0400

    take out dev console logs and other comments

--- src/components/PreviewControls/PreviewControls.js
+++ src/components/PreviewControls/PreviewControls.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import './PreviewControls.css'
 import { Link } from 'react-router-dom'
-// import Previews from '../../routes/Previews/Previews';
 
 const PreviewControls = ({ prevList, displayId, previewClick }) => {
 
     let renderPreviews = () => {
 
-        // let prevClick = this.props.previewClick
         return prevList.map(preview => {
             console.log(displayId)
             console.log(preview.id)
             if (preview.id != displayId.id) {
                 return (<li>
-                    <button 
-                    className='preview' 
-                    onClick={previewClick}
+                    <button
+                        className='preview'
+                        onClick={previewClick}
                     >
-                        <img 
-                        id={preview.id}
-                        src={preview.thumbnail_url} 
-                        alt="preview thumbnail" 
+                        <img
+                            id={preview.id}
+                            src={preview.thumbnail_url}
+                            alt="preview thumbnail"
                         />
                     </button>
                 </li>)
@@ -29,9 +27,9 @@ const PreviewControls = ({ prevList, displayId, previewClick }) => {
                     <li>
                         {/* {console.log('false was hit')} */}
                         <img src={preview.thumbnail_url} alt="preview thumbnail" />
-                    <button>
+                        <button>
                             set active
-                     </button>
+                        </button>
                     </li>
                 )
             }
@@ -42,14 +40,13 @@ const PreviewControls = ({ prevList, displayId, previewClick }) => {
     return (
         <div className="preview-controls">
             <ul className="preview-tiles">
+
                 {(!displayId) ? false : renderPreviews()}
 
-                {/* {console.log('preview controls is rendered')}
-            {console.log(prevList)} */}
             </ul>
-        <Link to='/creator'>
+            <Link to='/creator'>
                 add new
-        </Link>
+            </Link>
         </div>
     )
 }
--- src/routes/Previews/Previews.js
+++ src/routes/Previews/Previews.js
@@ -5,7 +5,7 @@ import YoutubeApiService from '../../services/youtube-api'
 import VideoStorage from '../../services/video-storage'
 import PreviewControls from '../../components/PreviewControls/PreviewControls'
 
-import MockYoutubeData from '../../Utils/mock-youtube-data'
+import MockYoutubeData from '../../Utils/mock-youtube-date'
 import pAPI from '../../services/previews-api'
 
 import { shuffle } from '../../Utils/Utils'
@@ -22,65 +22,15 @@ export default class Previews extends Component {
     }
     this.BASE_URL = 'http://localhost:8000/api'
     this.vidId = this.props.match.params.video_id;
-    //? BASE_URL/videos/vidId/previews
   }
 
 
   async componentDidMount() {
 
-
-    // let prevObj = {
-    //   video: {
-    //     title : 'testVideo', 
-    //     active_thumbnail_url: 'https://cf.ltkcdn.net/exercise/images/std/201862-675x450-pushups.jpg' , 
-    //     preview_count: 2, 
-    //     video_length: '10:01', 
-    //     youtube_display_name: 'test user' , 
-    //     youtube_url: 'https://www.youtube.com/watch?v=LYpE-heDZgE', 
-    //     tags:  ['fitness', 'push-ups', 'exercise'], 
-    //     user_id: 1,
-    //   }, 
-    //   previews: [
-    //     {
-    //       id: 1,
-    //       // created_at: ,
-    //       // updated_at: ,
-    //       thumbnail_url: 'https://ak5.picdn.net/shutterstock/videos/14428615/thumb/4.jpg',
-    //       video_id: 1,
-    //       is_active: true,
-    //       title: "test 1 (active start)",
-    //       description: "Follow our simple workout plan to get in the best shape of your life!"
-    //     },
-    //     {
-    //       id: 2,
-    //       // created_at: ,
-    //       // updated_at: ,
-    //       thumbnail_url: 'http://dream-gym.net/wp-content/uploads/2018/07/459880391.jpg',
-    //       video_id: 1,
-    //       is_active: false,
-    //       title: 'test 2 (inactive start)',
-    //       description: "Tired of failing results? Not looking for another fad? Try our push-up program!"
-    //     },
-    //   ]
-    // }
-
-    //? 
-    //? grab and set new public_user_video
-    //? const prevObj = await axios.get(`${this.BASE_URL}/videos/${this.vidId}/previews`)
-
     const prevObj = await pAPI.getPreviews(this.vidId)
-
-    console.log(prevObj)
+    // console.log(prevObj)
 
     VideoStorage.saveKey('laconic_current_video', prevObj.video);
-    //?----------------------
-    const video = JSON.parse(window.localStorage.getItem('laconic_current_video'))
-    console.log(video)
-
-    // const results = await pAPI.getPreviews(this.vidId)
-    // console.log(results)
-    // ? const results = 
-    // console.log(results)
 
     let findSelect = () => {
       let select = null
@@ -98,18 +48,16 @@ export default class Previews extends Component {
         return select
       }
     }
-
     let selected = findSelect()
 
-    console.log(selected)
-    console.log(prevObj.video)
-    console.log([...prevObj.previews])
+        // const results = await YoutubeApiService.search(video.tags)
+        const results = MockYoutubeData
+
     this.setState({
       video: prevObj.video,
       vidPreviews: [...prevObj.previews],
       selectedPrev: selected,
-      youtubeSearchResults: [...MockYoutubeData]
-      //  ? will be in production youtubeSearchResults: [...results],
+      youtubeSearchResults: [...results]
     })
     console.log(this.state)
   }
@@ -117,28 +65,21 @@ export default class Previews extends Component {
 
   renderPreviews = () => {
     const { selectedPrev, youtubeSearchResults, video } = this.state
-
-
     video.title = selectedPrev.title
     video.thumbnail_url = selectedPrev.thumbnail_url
 
     const videos = [video, ...youtubeSearchResults]
-    //? shuffle(videos) goes here
     shuffle(videos)
+    
     return videos.map((video, i) => {
       return <YoutubeSearchResult {...video} key={i} />
     })
   }
 
   previewClick = (e) => {
-    console.log(e.target.id)
-
     let selected = this.state.vidPreviews.find(preview => {
       return preview.id == e.target.id
     })
-
-
-    console.log(selected)
     this.setState({
       selectedPrev: selected
     })
