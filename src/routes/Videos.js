23aecf550c9ad715a814f38d73de5876330fb9a9
David Nordeen <davidnordeen@gmail.com>
2019-07-10 12:20:44 -0700

    handled post

--- src/routes/Videos.js
+++ src/routes/Videos.js
@@ -5,11 +5,16 @@ import VideoService from '../services/video-api';
 import AddVideos from '../components/AddVideo/AddVideo';
 import VideoStorage from '../services/video-storage';
 
+
 export default class Videos extends Component {
 
   state = {
     videos: [],
     current: [0, 3], //index of selected videos
+    title: '',
+    tags: '',
+    video_length: '',
+    youtube_display_name: '',
   }
 
   async componentDidMount() {
@@ -17,52 +22,48 @@ export default class Videos extends Component {
     this.setState({ videos })
   }
 
-  renderVideos(){
+  renderVideos() {
     const { videos } = this.state;
     //slice out vides that are needed from state
     const videoList = videos.slice(this.state.current[0], this.state.current[1] + 1)
-    return videoList.map(video => <VideoItem video={video} key={video.id}/>)
+    return videoList.map(video => <VideoItem video={video} key={video.id} />)
   }
   //show next four
   showNextFourVideos = e => {
-      if(this.state.current === 0) { return }
-      this.setState({
-        current: [this.state.current[0] + 4, this.state.current[1] + 4]
-      })
+    if (this.state.current === 0) { return }
+    this.setState({
+      current: [this.state.current[0] + 4, this.state.current[1] + 4]
+    })
   }
 
   showLastFourVideos = e => {
-      e.preventDefault()
-      // if(this.state.current === 0) { return }
-      this.setState({
-        current: [this.state.current[0] - 4, this.state.current[1] - 4]
-      })
+    e.preventDefault()
+    // if(this.state.current === 0) { return }
+    this.setState({
+      current: [this.state.current[0] - 4, this.state.current[1] - 4]
+    })
   }
 
 
 
   tagStringToArray = str => {
-   const tagsArr = str.split(', ').filter(Boolean);
-   if (tagsArr.length > 3) {
-     let threeTags = tagsArr.slice(1,4)
-     return threeTags;
-   }
-   return tagsArr;
+    const tagsArr = str.split(', ').filter(Boolean).slice(0, 3);
+    return tagsArr;
   }
-  
+
   errorCheck = (video) => {
-   for (let key in video) {
-     if (key === 'tags') {
-       if (video[key][0].trim() === "") {
-         return { status: true, message: 'Invalid tags' }
-       }
-     } else {
-       if (video[key].trim() === "") {
-         return { status: true, message: `${key} is required` }
-       }
-     }
-   }
-   return { status: false }
+    for (let key in video) {
+      if (key === 'tags') {
+        if (video[key][0].trim() === "") {
+          return { status: true, message: 'Invalid tags' }
+        }
+      } else {
+        if (video[key].trim() === "") {
+          return { status: true, message: `${key} is required` }
+        }
+      }
+    }
+    return { status: false }
   }
   errorHandler = err => {
     this.setState({ error: err.message })
@@ -75,8 +76,8 @@ export default class Videos extends Component {
       title: this.state.title,
       video_length: this.state.video_length,
       youtube_display_name: this.state.youtube_display_name,
-     tags: this.tagStringToArray(this.state.tags),
-     // ? will be changed when utils is updated
+      tags: this.tagStringToArray(this.state.tags),
+      // ? will be changed when utils is updated
     }
     const isError = this.errorCheck(video);
     if (isError.status === true) {
@@ -85,6 +86,7 @@ export default class Videos extends Component {
     }
     VideoStorage.saveKey('laconic_current_video', video)
     this.props.history.push('/creator')
+    VideoService.postVideo(video);
   }
 
 
@@ -97,7 +99,6 @@ export default class Videos extends Component {
   }
 
 
-
   render() {
     return (
       <section className='videos-page'>
@@ -108,11 +109,11 @@ export default class Videos extends Component {
             handleSubmit={this.handleSubmit} />
         </div>
         <div className="btn-container">
-          <button type="button" disabled={!this.state.current[0]} onClick ={this.showLastFourVideos}>Previous</button>
-          <button type="button" onClick ={this.showNextFourVideos}>Next</button>
+          <button type="button" disabled={!this.state.current[0]} onClick={this.showLastFourVideos}>Previous</button>
+          <button type="button" onClick={this.showNextFourVideos}>Next</button>
         </div>
         <div className='my-videos-container'>
-        {this.state.videos.length ? this.renderVideos() : ''}
+          {this.state.videos.length ? this.renderVideos() : ''}
         </div>
       </section>
     );
