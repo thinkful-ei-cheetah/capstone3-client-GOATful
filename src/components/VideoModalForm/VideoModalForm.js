f30f7ecfea1351a36347fb3f9e8c59f46d48cee4
Michael Verdi <michael.v.verdi@gmail.com>
2019-07-17 14:26:24 -0400

    refactor video form to use single modal and remove dup logic

---
+++ src/components/VideoModalForm/VideoModalForm.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react'
+import Modal from 'react-modal';
+import AddVideos from '../AddVideo/AddVideo';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons'
+import VideoStorage from '../../services/video-storage';
+import VideoService from '../../services/video-api';
+
+import { 
+  checkTime,
+  tagStringToArray,
+  errorCheckNewVideo,
+  formatDuration
+} from '../../Utils/Utils'
+
+const modalStyles = {
+  content : {
+    maxWidth              : '80%',
+    width                 : '350px',
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#root')
+
+export default class VideoModalForm extends Component {
+  state = {
+    title: this.setTitle(),
+    tags: this.setTags(),
+    video_length: this.setVideoLength(),
+    youtube_display_name: this.setYoutubeDisplayName(),
+    addError: '',
+  }
+
+  setTitle() {
+    if (this.props.action === 'edit') return this.props.video.title
+    return ''
+  }
+
+  setTags() {
+    if (this.props.action === 'edit') return this.props.video.tags
+    return ''
+  }
+
+  setVideoLength() {
+    if (this.props.action === 'edit') return formatDuration(this.props.video.video_length)
+    return ''
+  }
+
+  setYoutubeDisplayName() {
+    if (this.props.action === 'edit') return this.props.video.youtube_display_name
+    return ''
+  }
+
+  handleFields = e => {
+    let { value, name } = e.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const {title, video_length, youtube_display_name, tags} = this.state
+    
+    const checkedTime = checkTime(video_length);
+    if (checkedTime.error){
+      this.addErrorHandler(checkedTime)
+      return;
+    }
+
+    const video = {
+      title,
+      youtube_display_name,
+      video_length: checkedTime.googleTimeString,
+      tags: Array.isArray(tags) ? tags : tagStringToArray(tags),
+    }
+
+    const isError = errorCheckNewVideo(video);
+    if (isError.status === true){
+      this.addErrorHandler(isError)
+      return
+    }
+    try {
+      if (this.props.action === 'new') {
+        const createdVideo = await VideoService.postVideo(video);
+        VideoStorage.saveKey('laconic_current_video', createdVideo)
+        this.props.history.push('/creator')
+      } else {
+        await VideoService.patchVideo(this.props.video.id, video)
+        this.props.getVideoList()
+        this.props.onRequestClose()
+      }
+    } catch(e){
+      this.addErrorHandler(e)
+    }
+  } 
+
+  addErrorHandler(err) {
+    this.setState({addError: err.error || err.message})
+  }
+
+  renderFormTitle = () => {
+    if (this.props.action === 'edit') return 'Edit Video Project'
+    return 'Add New Video Project'
+  }
+
+  render() {
+    return (
+      <Modal
+      isOpen={this.props.isOpen}
+      onRequestClose={this.props.onRequestClose}
+      style={modalStyles}
+      contentLabel={'Video Modal Form'}
+      closeTimeoutMS={200}
+        >
+          <h2 className="add-video-header">{this.renderFormTitle()}</h2>
+          <span className='close-modal-btn' onClick={this.props.onRequestClose}>
+            <FontAwesomeIcon 
+              icon={faWindowClose}
+            />
+          </span>
+          <AddVideos
+            fields={{...this.state}}
+            handleFields={this.handleFields}
+            handleSubmit={this.handleSubmit}
+            action={this.props.action}
+          />
+      </Modal>
+    )
+  }
+}
--- src/routes/Videos.js
+++ src/routes/Videos.js
@@ -2,83 +2,26 @@ import React, { Component } from 'react';
 import VideoItem from '../components/VideoItem/VideoItem';
 import './Videos.css';
 import Loader from '../components/Loader/Loader'
-import AddVideoModal from '../components/AddVideoModal/AddVideoModal'
+import VideoModalForm from '../components/VideoModalForm/VideoModalForm'
 import VideoService from '../services/video-api';
-import VideoStorage from '../services/video-storage';
 import FAB from '../components/FAB/FAB'
 import { withAppContext } from '../contexts/AppContext'
-
-import { checkTime, tagStringToArray, errorCheckNewVideo } from '../Utils/Utils'
 
 class Videos extends Component {
 
   state = {
     videos: [],
     isLoading: true,
-    videoEditError: null,
-    title: '',
-    tags: '',
-    video_length: '',
-    youtube_display_name: '',
-    addError: '',
     modalIsOpen: false,
   }
 
-  errorHandler = err => {
-    this.setState({videoEditError: err.error || err.message})
-    setTimeout(()=>this.setState({videoEditError: null}), 3000)
-  }
-
-  addErrorHandler = err => {
-    this.setState({addError: err.error || err.message})
-    setTimeout(()=>this.setState({addError: null}), 3000)
-  }
-
   async componentDidMount() {
-    try {
-      const videos = await VideoService.getVideos();
-      this.setState({ videos, isLoading: false })
-    } catch(err) {
-      this.setState({ isLoading: false }, this.props.appContext.setAppError(err.message))
-    }
-  }
-
-  async updateSelectedVideo(id, updates) {
-    try{
-      await VideoService.patchVideo(id, updates)
-      const videos = await VideoService.getVideos();
-      this.setState({ videos })
-    } catch(e){ 
-      this.errorHandler(e)
-    }
+    this.getVideoList()
   } 
-
-  handleFormSubmission = async (id, values) => {
-    const checkedTime = checkTime(values.video_length);
-    if (checkedTime.error){
-      this.errorHandler(checkedTime)
-      return;
-    }
-
-    const updateVideo = {
-      title: values.title,
-      video_length: checkedTime.googleTimeString,
-      youtube_display_name: values.youtube_display_name,
-      tags: tagStringToArray(values.tags),
-    }
-
-    const isError = errorCheckNewVideo(updateVideo);
-    if (isError.status === true){
-      this.errorHandler(isError)
-      return
-    }
-    this.updateSelectedVideo(id, updateVideo);
-  }
 
   deleteVideo = async videoId => {
     try{
       this.setState({isLoading: true })
-
       await VideoService.deleteVideo(videoId)
       const videos = await VideoService.getVideos();
       this.setState({ videos, isLoading: false })
@@ -97,71 +40,24 @@ class Videos extends Component {
 
   renderVideos() {
     const { videos } = this.state;
-    return videos.map(video => <VideoItem
-      handleFormSubmission={this.handleFormSubmission}
-      deleteVideo={this.deleteVideo}
-      video={video}
-      key={video.id}
-      formError = {this.state.videoEditError}
-      />)
+    return videos.map(video => 
+      <VideoItem
+        deleteVideo={this.deleteVideo}
+        video={video}
+        key={video.id}
+        getVideoList={this.getVideoList}
+      />
+    )
   }
 
-  errorCheckNewVideo = (video) => {
-    for (let key in video) {
-      if (key === 'tags') {
-        if (video[key][0].trim() === "") {
-          return { status: true, message: 'Invalid tags' }
-        }
-      } else {
-        if (video[key].trim() === "") {
-          return { status: true, message: `${key} is required` }
-        }
-      }
-    }
-    return { status: false }
-  }
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const values = {
-      title: this.state.title,
-      video_length: this.state.video_length,
-      youtube_display_name: this.state.youtube_display_name,
-      tags: this.state.tags,
-    }
-    
-    const checkedTime = checkTime(values.video_length);
-    if (checkedTime.error){
-      this.addErrorHandler(checkedTime)
-      return;
-    }
-
-    const newVideo = {
-      title: values.title,
-      video_length: checkedTime.googleTimeString,
-      youtube_display_name: values.youtube_display_name,
-      tags: tagStringToArray(values.tags),
-    }
-
-    const isError = errorCheckNewVideo(newVideo);
-    if (isError.status === true){
-      this.addErrorHandler(isError)
-      return
-    }
+  getVideoList = async () => {
+    this.setState({isLoading: true})
     try {
-     const createdVideo = await VideoService.postVideo(newVideo);
-      VideoStorage.saveKey('laconic_current_video', createdVideo)
-      this.props.history.push('/creator')
-    } catch(e){
-      this.addErrorHandler(e)
+      const videos = await VideoService.getVideos();
+      this.setState({ videos, isLoading: false })
+    } catch(err) {
+      this.setState({ isLoading: false }, this.props.appContext.setAppError(err.message))
     }
-  }
-
-  handleFields = e => {
-    let { value, name } = e.target;
-    this.setState({
-      [name]: value
-    })
   }
 
   render() {
@@ -169,13 +65,19 @@ class Videos extends Component {
       <section className='videos-page page'>
         <Loader isLoading={this.state.isLoading} />
         <FAB onClick={this.openModal}/>
-        <AddVideoModal 
-          fields={this.state}
-          handleFields={this.handleFields}
-          handleSubmit={this.handleSubmit}
+        <VideoModalForm
+          history={this.props.history}
           isOpen={this.state.modalIsOpen}
           onRequestClose={this.closeModal}
+          action='new'
         />
+        <h2>Your Video Dashboard</h2>
+        <div className='filter-container'>
+          <form id='filter-videos'>
+            <label htmlFor='title'>Filter by title</label>
+            <input type='text' name='title' id='title'/>
+          </form>
+        </div>
         <div className='my-videos-container'>
           {this.state.videos.length ? this.renderVideos() : <p className="no-videos-text">You currently have 0 videos</p>}
         </div>
