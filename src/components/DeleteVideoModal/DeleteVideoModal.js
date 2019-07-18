8dfa2c17f60c790f59c8271b5906f068a4467934
Kristof-Pierre Cummings <kristofpierre8@gmail.com>
2019-07-18 12:15:43 -0500

    completed delete modal styling

--- src/components/DeleteVideoModal/DeleteVideoModal.js
+++ src/components/DeleteVideoModal/DeleteVideoModal.js
@@ -32,31 +32,31 @@ export default class DeleteVideoModal extends Component {
     if (video.preview_count > 0) {
       return video.preview_count === 1 ? `1 preview will also be deleted.` : `${video.preview_count} previews will also be deleted.`
     }
-    return ''
+    return <p className='blank-space-delete'></p>
   }
 
   render() {
     const {video} = this.props
     return (
-      <Modal
+      <Modal className="delete-video-modal"
         isOpen={this.props.isOpen}
         onRequestClose={this.props.onRequestClose}
         style={modalStyles}
         contentLabel={'Delete Video'}
         closeTimeoutMS={200}
       >
-        <h2 className="add-video-header">Are you sure?</h2>
+        <h2 className="delete-video-header">Are you sure?</h2>
         <p>Delete {video.title}?</p>
         <p>{this.renderPreviewCount(video)}</p>
         
         <span className='close-modal-btn' onClick={this.props.onRequestClose}>
           <FontAwesomeIcon 
-            icon={faWindowClose}
+            icon={faWindowClose} 
           />
         </span>
-        <div className= "button-controls">
-          <button className="cancel" onClick={this.props.onRequestClose}>Cancel</button>
-          <button className="confirm" onClick={this.props.handleDelete}>Delete</button>
+        <div className= "delete-button-controls">
+          <button className="cancel-btn" onClick={this.props.onRequestClose}>Cancel</button>
+          <button className="confirm-btn" onClick={this.props.handleDelete}>Delete</button>
         </div>
       </Modal>
     )
