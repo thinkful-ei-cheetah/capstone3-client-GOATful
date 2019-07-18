3a261f42a78a379cd53d046050d27baa0f26b179
elangreen <green.elan5@gmail.com>
2019-07-18 13:29:36 -0700

    fixed navbar responsiveness and added conditional rendering on the my videos link

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
 import TokenService from '../../services/token-service'
 import UserContext from '../../contexts/UserContext'
 import Logo from '../Logo/Logo'
@@ -20,9 +21,9 @@ class NavBar extends Component {
         </span>
         <nav className='stroke'>
           <div className='navLinks'>
-          <Link className="videosLink" to='/videos'>
+          {this.props.location.pathname !== "/videos" && <Link className="videosLink" to='/videos'>
            My Videos
-          </Link>
+          </Link>}
           <Link
             className="logout link"
             onClick={this.handleLogoutClick}
@@ -49,6 +50,7 @@ class NavBar extends Component {
   }
 
   render() {
+    console.log(this.props)
     return (
       <header>
         <div className="title-container">
@@ -67,4 +69,4 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar
+export default withRouter(NavBar)
