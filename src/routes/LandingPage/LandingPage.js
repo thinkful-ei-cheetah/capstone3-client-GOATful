f0fee39a229f1a173e08e932f84918df5e6a649a
David Nordeen <davidnordeen@gmail.com>
2019-07-18 14:24:09 -0700

    need responsive styling

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-// import './LandingPage.css'
+import './LandingPage.css'
 import CTR from '../../img/CTRwide.jpg'
 import AB from '../../img/ABwide.png'
 import YT from '../../img/youtubewide.jpg'
@@ -23,14 +23,14 @@ class Landing extends Component {
           </div>
         </section>
           <section className='about'>
-            <figure>
+            <figure className="images">
               <img className="AboutImage" src={YT} alt="YouTube" />
             </figure>
             <div className="AboutText">
               <h3>
                 What we do
             </h3>
-              <p>
+              <p className="p">
                 We Preview your thumbnails, so you can see what they will look like in Youtube's styling in both desktop and App view.
                 Save your thumbnails so you can easily see the difference between the two.
                 Keep all your thumbnail Ideas in one place!
@@ -49,30 +49,30 @@ class Landing extends Component {
             <h3>
               So What Is Click-Through Rate, Anyway?
             </h3>
-            <p>
+                <p className="p">
               CTR is the rate at which your videos are clicked. This number is the percentage of people who view your video (impressions) and then actually go on to click the video (clicks). The formula for CTR looks like this:
             </p>
             <h4>
               (Total Clicks on Video) / (Total Impressions) = Click Through Rate
             </h4>
-            <p>
+                <p className="p">
               Generally, you can view your click-thru rate within the dashboard of your YouTube account. A high CTR means that a high percentage of people who see your video click it.
             </p>
             </div>
-            <figure>
+              <figure className="images">
               <img className="CTRimage"  src={CTR} alt="Click through rate" />
             </figure>
           </section>
 
           <section className='AB-testing'>
-            <figure>
+              <figure className="images">
               <img className="ABimage" src={AB} alt="A/B Testing" />
             </figure>
             <div className="ABtext">
             <h3>
                A/B Testing
             </h3>
-            <p>
+                <p className="p">
               An “A/B Test” is a test where 2 variants of something (anything) is tested for equal amounts of time, to see which version performs the best.
               In this case, what is being tested and compared is 2 versions of a thumbnail, of your own choosing. By uploading 2 thumbnails into the system, they are changed each day automatically on your channel (for a chosen period), and by the end of the test we automatically gather the data from your YouTube Analytics and see which of the thumbnails resulted in the most views.
             </p>
