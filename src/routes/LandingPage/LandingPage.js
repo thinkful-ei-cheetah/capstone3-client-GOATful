aa1dfec42b41bff3a2c3d8c5150d21c035c7f7b5
Michael Verdi <michael.v.verdi@gmail.com>
2019-07-19 13:26:47 -0400

    styled landing page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import './LandingPage.css'
-import CTR from '../../img/CTRwide.jpg'
-import AB from '../../img/ABwide.png'
-import YT from '../../img/youtubewide.jpg'
+import './laconic-bg-2.jpg'
+import './dark-hero-bg.png'
+import './aside-bg.png'
+import DemoOnTable from './demo-bg.png'
 
 class Landing extends Component {
   pushToLogin = () => {
@@ -13,60 +14,43 @@ class Landing extends Component {
       <>
       <main className="landing-page-container">
         <section className='hero page'>
-          <h1 className="slogan">
-            Let your thumbnail do the talking.
-          </h1>
+          <div className='hero-content'>
+            <h2>Youtube Creators</h2>
+            <h3>Ready to 10x your view counts?</h3>
+
+            <p>Laconic is the only tool that helps you optimize the look and feel of your video thumbnails before you post to YouTube.</p>
+            <button onClick={this.pushToLogin} className='create-laconic'>
+              Create with Laconic
+            </button>
+          </div>
         </section>
         <section className='about'>
-          <div className="AboutText">
-            <h3>
-              What We Do
-            </h3>
-            <p className="p">
-              Laconic allows you to see what your YouTube thumbnails will look like before you publish them. Save and compare multiple thumbnails related to a video. When you decide which thumbnail to use, copy the fields directly from Laconic and publish your video!  
-          </p>
-            <div className="btn-container">
-              <button onClick={this.pushToLogin} className='create-laconic'>
-                Create with Laconic
-          </button>
-
+          <div className='left'>
+            <div className="AboutText">
+              <h3>
+                What We Do
+              </h3>
+              <p className="p">
+                Laconic allows you to see what your YouTube thumbnails will look like before you publish them. Save and compare multiple thumbnails related to a video. When you decide which thumbnail to use, copy the fields directly from Laconic and publish your video!  
+            </p>
+              <div className="btn-container">
+                <button onClick={this.pushToLogin} className='create-laconic'>
+                  Create with Laconic
+                </button>
+              </div>
             </div>
           </div>
-        </section>
-        <section className='click-through'>
-          <h3>
-            What Is Click-Through Rate?
-          </h3>
-          <figure className="images">
-            <img className="CTRimage"  src={CTR} alt="Click through rate" />
-          </figure>
-          <div className="CTRtext">
-              <p className="p">
-            CTR is the rate at which your videos are clicked. This number is the percentage of people who view your video (impressions) and then actually go on to click the video (clicks). The formula for CTR looks like this:
-          </p>
-          <h4>
-            (Total Clicks on Video) / (Total Impressions) = Click Through Rate
-          </h4>
-              <p className="p">
-            Generally, you can view your click-thru rate within the dashboard of your YouTube account. A high CTR means that a high percentage of people who see your video click it.
-          </p>
+          <div className='right'>
+            <img src={DemoOnTable} alt='Laconic on mac'/>
           </div>
         </section>
-        <section className='AB-testing'>
-          <h3>
-              A/B Testing
-          </h3>
-          <figure className="images">
-            <img className="ABimage" src={AB} alt="A/B Testing" />
-          </figure>
-          <div className="ABtext">
-    
-              <p className="p">
-            An “A/B Test” is a test where 2 variants of something (anything) is tested for equal amounts of time, to see which version performs the best.
-            In this case, what is being tested and compared is 2 versions of a thumbnail, of your own choosing. By uploading 2 thumbnails into the system, they are changed each day automatically on your channel (for a chosen period), and by the end of the test we automatically gather the data from your YouTube Analytics and see which of the thumbnails resulted in the most views.
-          </p>
+        
+        <aside>
+          <div className='quote'>
+            Since using Laconic my total views and subscriptions have sky rocketed!
           </div>
-        </section>
+        </aside>
+        
       </main>
       </>
     );
